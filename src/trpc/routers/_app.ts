a81c4b824d5d5759a3535eb558b7239bffa8da25
Raid Thabet <thabet.raid123@gmail.com>
2025-03-29 14:09:59 +0100

    Added auth to tRPC context, protected procedure, and rate limiting

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,22 @@
 import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import {baseProcedure, createTRPCRouter, protectedProcedure} from '../init';
+// import {auth} from "@clerk/nextjs/server";
 // import {TRPCError} from "@trpc/server";
 export const appRouter = createTRPCRouter({
-    hello: baseProcedure
+    hello: protectedProcedure
         .input(
             z.object({
                 text: z.string(),
             }),
         )
-        .query((opts) => {
-            // throw new TRPCError({code: "BAD_REQUEST"})
+        .query(async (opts) => {
+            // does not make sense to do this every single time
+            // const {userId} = await auth();
+            // console.log(userId);
+
+            // console.log(opts.ctx.clerkUserId);
+            console.log({dbUser: opts.ctx.user})
+
             return {
                 greeting: `hello ${opts.input.text}`,
             };
