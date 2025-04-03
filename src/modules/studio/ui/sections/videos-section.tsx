1cef81023dab68daab02acf99546340236737a2c
Raid Thabet <thabet.raid123@gmail.com>
2025-04-03 12:45:20 +0100

    Updated videos schema, assigned video thumbnail and preview, and handled video.asset.ready event

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -7,6 +7,7 @@ import {ErrorBoundary} from "react-error-boundary";
 import InfiniteScroll from "@/components/infinite-scroll";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 import Link from "next/link";
+import VideoThumbnail from "@/modules/videos/ui/components/video-thumbnail";
 
 export function VideosSection() {
     return (
@@ -45,7 +46,16 @@ export function VideosSectionSuspense() {
                             <Link href={`/studio/video/${video.id}`} key={video.id} legacyBehavior>
                                 <TableRow className={"cursor-pointer"}>
                                     <TableCell>
-                                        {video.title}
+                                        <div className="flex items-center gap-4">
+                                            <div className="relative aspect-video w-36 shrink-0">
+                                                <VideoThumbnail
+                                                    imageUrl={video.thumbnailUrl}
+                                                    previewUrl={video.previewUrl}
+                                                    title={video.title}
+                                                    duration={video.duration || 0}
+                                                />
+                                            </div>
+                                        </div>
                                     </TableCell>
                                     <TableCell>
                                         Visibility
---
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Image from "next/image";
+import {formatDuration} from "@/lib/utils";
+
+type Props = {
+    imageUrl?: string | null;
+    previewUrl?: string | null;
+    title: string;
+    duration: number;
+}
+
+function VideoThumbnail({imageUrl, previewUrl, title, duration}: Props) {
+    return (
+        <div className={"relative group"}>
+            {/* Thumbnail wrapper */}
+            <div className="relative w-full overflow-hidden rounded-xl aspect-video">
+                <Image
+                    src={imageUrl ?? "/placeholder.svg"}
+                    alt={title}
+                    fill
+                    className={"size-full object-cover group-hover:opacity-0"}
+                />
+                <Image
+                    src={previewUrl ?? "/placeholder.svg"}
+                    alt={title}
+                    fill
+                    className={"size-full object-cover opacity-0 group-hover:opacity-100"}
+                />
+            </div>
+
+            {/* Video duration box */}
+            <div className="absolute bottom-2 right-2 px-1 py-0.5 rounded bg-black/80 text-white text-xs font-medium">
+                {formatDuration(duration)}
+            </div>
+        </div>
+    );
+}
+
+export default VideoThumbnail;
